e39744b8ffb856ef3900e71a02c1530d462a9350
blackbarth <blackbarth@outlook.com>
2019-03-25 20:20:26 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -8,16 +8,12 @@ public class Department implements Serializable {
 
 	private Integer id;
 	private String name;
-
+	
 	public Department() {
 	}
 
 	public Department(Integer id, String name) {
 		this.id = id;
-		this.name = name;
-	}
-
-	public Department(String name) {
 		this.name = name;
 	}
 
